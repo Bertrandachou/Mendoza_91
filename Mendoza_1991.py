245941ee3a5dbfece6ccbf75e1936966c28a2743
Bertrand Achou <bertrandachou@orange.fr>
2013-05-14 15:35:28 +0200

    Third version
    The problem of negative consumption is fixed.
    However it appears that the value function is not always increasing in k
    (which seems strange and is certainly a mistake).
    This is likely to come from the max operator or the computation of EV0.
    However I would favor the first hypothesis.
    This is tomorrow's job.

--- Mendoza_1991.py
+++ Mendoza_1991.py
@@ -18,15 +18,15 @@ import time
 
 # First, we define our grid of parameters
 
-p = {'alpha': 0.32, 'rstar': 0.04, 'gamma': 1.001 , 'delta': 0.1, 'omega': 1.455, 'beta': 0.11, 'phi': 0}
+p = {'alpha': 0.32, 'rstar': 0.04, 'gamma': 1.001 , 'delta': 0.1, 'omega': 1.455, 'beta': 0.11, 'phi': 0, 'epsilon': 10**(-6) }
 
 # Then the grids for the stochastic process (transition and values)
 
-p_stoch = {'e': 1.18, 'n': 0, 'rho': 0.36, 'rho_en': 0}
+p_stoch = {'e': 0.0118, 'n': 0, 'rho': 0.36, 'rho_en': 0}
 
 # We define the parameters of the grids
 
-pgrid = {'kmin': 3.25 , 'kmax': 3.56 , 'nk': 10, 'Amin': -1.42 , 'Amax': 0.08 , 'nA': 12 }
+pgrid = {'kmin': 3.25 , 'kmax': 3.56 , 'nk': 22, 'Amin': -1.42 , 'Amax': 0.08 , 'nA': 22 }
 
 
 # We build the transition matrix that will be used
@@ -73,7 +73,7 @@ Apgrid       = np.reshape( Apgrid_temp3 , (4 , pgrid['nA'], pgrid['nA'] , pgrid[
 
 egrid = np.zeros ( (4 , pgrid['nA'], pgrid['nA'] , pgrid['nk'] , pgrid['nk']) )
 egrid[0:2].fill(p_stoch['e'])
-egrid[2:].fill(-p_stoch['e'])
+egrid[2:].fill(- p_stoch['e'])
 
 
 
@@ -87,30 +87,40 @@ ngrid[3].fill(-p_stoch['n'])
 
 # now we define our functions 
 
+import numexpr
+
 # a function that computes the optimal labor as a function of k,e and the parameters p
 
 def labour(k,e,p):
-    return ( (1 - p['alpha']) * np.exp(e) * (k**p['alpha']) ) ** (1 / (p['alpha'] + p['omega'] - 1))
+    alpha = p['alpha']
+    omega = p['omega']
+    return numexpr.evaluate( '( (1 - alpha) * exp(e) * (k**alpha) ) ** (1 / (alpha + omega- 1))' )
     
 
 # utility as a function of c, l and the parameters p
 
 def utility(c,l,p):
-    if p['gamma'] == 1:
-        return np.log( c - (l**p['omega'])/p['omega'] )
+    gamma = p['gamma']
+    omega = p['omega']
+    if gamma == 1:
+        return numexpr.evaluate( 'log( c - (l**omega)/omega )' )
     else:
-        return (( (c - (l**p['omega'])/p['omega'])**(1-p['gamma']) - 1 )) / (1 - p['gamma'])
+        return numexpr.evaluate( '( (c - (l**omega)/omega)**(1-gamma) - 1 ) / (1 - gamma)' )
     
 # a function which returns the discount factor from c, l, e and the parameters p
 
 def discount(c,l,p):
-    return np.exp(-p['beta'] * np.log(1 + c - (l**p['omega'])/p['omega'] ))
+    beta = p['beta']
+    omega = p['omega']
+    return numexpr.evaluate( 'exp(-beta * log(1 + c - (l**omega)/omega ))' )
     
     
 # production function as a function k, k', l, e and the parameters p
 
 def production(k,kp,l,e,p):
-    return np.exp(e) * (k**p['alpha']) * (l**(1-p['alpha'])) - (p['phi']/2) * (kp - k)**2
+    alpha = p['alpha']
+    phi   = p['phi']
+    return  numexpr.evaluate( 'exp(e) * (k**alpha) * (l**(1-alpha)) - (phi/2) * (kp - k)**2' )
     
 
 # we define our grid for value function
@@ -137,11 +147,15 @@ def new_value(k,kp,A,Ap,e,n,p,pgrid,transit,V0):
     # state and possible decision he can take
     # when consumption is negative we put it to 0.001 and assigne a large negative utility
     # so that it is never optimal for the agent to choose these values
+    delt = p['delta']
+    omeg = p['omega']
+    rs   = p['rstar']
     
     ltemp = labour(k,e,p)
-    ctemp = production(k,kp,ltemp,e,p) - kp + k * (1 - p['delta'])\
-    + (1 + p['rstar'] * np.exp(n)) * A - Ap
-    cltemp = ctemp - (ltemp**(p['omega']))/p['omega']
+    ctemp = production(k,kp,ltemp,e,p) \
+    + numexpr.evaluate( '-kp + k * (1 - delt) + (1 + rs * exp(n)) * A - Ap' )
+    
+    cltemp =  numexpr.evaluate( 'ctemp - (ltemp**(omeg))/omeg' )
     
     
     budget_not = (cltemp <= 0)
@@ -161,7 +175,7 @@ def new_value(k,kp,A,Ap,e,n,p,pgrid,transit,V0):
     
     TV0 = utemp + disc * EV0[:,None,:,:,None]
     
-    TV0[budget_not] = -99999999999
+    TV0[budget_not] = -9999999
 
     
     new_V0_temp = TV0.max(axis=3)
@@ -170,27 +184,36 @@ def new_value(k,kp,A,Ap,e,n,p,pgrid,transit,V0):
     return   new_V0.reshape( (4 , pgrid['nA'] * pgrid['nk'] ) )
     
 
-#TV = new_value(kgrid,kpgrid,Agrid,Apgrid,egrid,ngrid,p,pgrid,stoch_transit,V0)
+test = new_value(kgrid,kpgrid,Agrid,Apgrid,egrid,ngrid,p,pgrid,stoch_transit,V0)
 
 
-#test = new_value(kgrid,kpgrid,Agrid,Apgrid,egrid,ngrid,p,pgrid,stoch_transit,V0)
-
-t1 = time.time()
-
-for i in xrange(300):
+def find_value(kgrid,kpgrid,Agrid,Apgrid,egrid,ngrid,p,pgrid,stoch_transit,V0):
     
-    TV = new_value(kgrid,kpgrid,Agrid,Apgrid,egrid,ngrid,p,pgrid,stoch_transit,V0)
+    crit = 10
+    iteration = 0
+
+    while crit> p['epsilon']:
+        
+        iteration = iteration + 1       
     
-    #print abs(V0 - TV).max()
+        TV = new_value(kgrid,kpgrid,Agrid,Apgrid,egrid,ngrid,p,pgrid,stoch_transit,V0)
     
-    V0 = TV
+        crit = abs(V0 - TV).max()
+        # print iteration
+        # print crit
+    
+        V0 = TV
+    
+    return V0
+        
+#%prun -s time find_value(kgrid,kpgrid,Agrid,Apgrid,egrid,ngrid,p,pgrid,stoch_transit,V0)
 
-t2 = time.time()
+Value = find_value(kgrid,kpgrid,Agrid,Apgrid,egrid,ngrid,p,pgrid,stoch_transit,V0)
 
-print (t2-t1)    
-#import pylab
+import pylab
 
-#y = V0[0,0:pgrid['nk']].reshape((1,10))
+Vplot = Value[0,20:(pgrid['nk']+20)].reshape(pgrid['nk'])
+kplot = klin.reshape(pgrid['nk'])
 
-#pylab.plot( klin , y )
+pylab.plot(kplot,Vplot)
 
